b82a2243f5b4017bbb6ed48cbc21308e01e0d82e
Alex Toney <toneyalex@gmail.com>
2012-04-03 22:15:08 -0700

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+
+from setuptools import setup, find_packages
+
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+setup(
+    name='magnum',
+    version='0.1',
+    description='Python ORM for Humans.',
+    author='Bradford Toney',
+    author_email='bradford.toney@gmail.com',
+    packages=find_packages(),
+    package_data={'': ['LICENSE', 'NOTICE']},
+    include_package_data=True,
+)
